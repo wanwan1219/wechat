2b5a561de405ad8e6b8f2ee09a8bf9c158d70c3e
wanwan1219 <liwan_1219@126.com>
2018-01-14 17:12:17 +0800

    微信小程序

---
+++ swiper/index/index.js
@@ -0,0 +1,15 @@
+page({
+  data:{
+    autoplay:true,
+    interval:2000,
+    duration:1000,
+    circular:true,
+    imgUrl:[
+      "../img/ban1.jpg",
+      "../img/ban2.jpg",
+      "../img/ban3.jpg",
+      "../img/ban4.jpg",
+      "../img/ban5.jpg"
+    ]
+  }
+})
